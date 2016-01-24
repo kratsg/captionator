32531d6e3a2427ab24e98491e7824d9ab893cdb4
Giordon Stark <kratsg@gmail.com>
2016-01-24 15:32:32 -0600

    add YoR prelim. No slides yet. Most of the infrastructure is in place now.

--- app.js
+++ app.js
@@ -24,6 +24,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next){
+    req.yaml = yaml;
+    req.fs = fs;
+    next();
+});
+
 app.use('/', routes);
 
 // catch 404 and forward to error handler
@@ -38,6 +44,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
+  app.locals.pretty = true;
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Captionator' });
 });
 
-router.get('/:play_name', function(req, res, next) {
-    res.render('play', {corpus: yaml.safeLoad(fs.readFileSync('./data/'+req.params.play_name+'.yml'))});
+router.get('/plays', function(req, res, next) {
+    req.fs.readdir('./data/', function(err, plays){
+        if(err) throw err;
+        plays = plays.filter(function(play){ return play.substr((~-play.lastIndexOf(".") >>> 0)+2) == "yml"; })
+                     .map(function(play){ return play.replace('.yml', '') });
+        res.render('plays', {plays: plays});
+    });
+});
+
+router.get('/play/:play_name', function(req, res, next) {
+    res.render('play', {corpus: req.yaml.safeLoad(req.fs.readFileSync('./data/'+req.params.play_name+'.yml')), title: req.params.play_name.replace(/_/g, ' ')});
 });
 
 module.exports = router;
