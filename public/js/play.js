b5bfc5e5a9cfd674609857b0b12e1779c9f03a5b
Giordon Stark <kratsg@gmail.com>
2016-01-24 22:12:54 -0600

    add basic functionality to move around the slides

--- public/js/play.js
+++ public/js/play.js
@@ -1,1 +1,24 @@
-(function(){return 0;})();
+(function(){
+    var moveForward = function(){
+        var currSlide = $('.slide.active');
+        var nextSlide = currSlide.next('.slide');
+        if(nextSlide && nextSlide.length){
+            currSlide.removeClass('active');
+            nextSlide.addClass('active');
+        }
+        return false;
+    };
+
+    var moveBackward = function(){
+        var currSlide = $('.slide.active');
+        var prevSlide = currSlide.prev('.slide');
+        if(prevSlide && prevSlide.length){
+            currSlide.removeClass('active');
+            prevSlide.addClass('active');
+        }
+        return false;
+    };
+
+    Mousetrap.bind(['right', 'down', 'space'], moveForward);
+    Mousetrap.bind(['left', 'up', 'backspace'], moveBackward);
+})();
