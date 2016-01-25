23307498f6f3328f0c87ea8c5968dc82dc6d3756
Giordon Stark <kratsg@gmail.com>
2016-01-24 22:57:05 -0600

    add playTitle as well as blank screen functionality

--- public/js/play.js
+++ public/js/play.js
@@ -1,10 +1,16 @@
 (function(){
+    var playTitle = window.playName.replace(/_/g, ' ');
+    var pushState = function(index){
+        history.pushState({}, playTitle+": slide "+index, "/"+index);
+    };
+
     var moveForward = function(){
         var currSlide = $('.slide.active');
         var nextSlide = currSlide.next('.slide');
         if(nextSlide && nextSlide.length){
             currSlide.removeClass('active');
             nextSlide.addClass('active');
+            pushState(nextSlide.index());
         }
         return false;
     };
@@ -15,10 +21,16 @@
         if(prevSlide && prevSlide.length){
             currSlide.removeClass('active');
             prevSlide.addClass('active');
+            pushState(prevSlide.index());
         }
         return false;
     };
 
+    var blankScreen = function(){
+        $('body').toggleClass('hide');
+    };
+
     Mousetrap.bind(['right', 'down', 'space'], moveForward);
     Mousetrap.bind(['left', 'up', 'backspace'], moveBackward);
+    Mousetrap.bind('b', blankScreen);
 })();
