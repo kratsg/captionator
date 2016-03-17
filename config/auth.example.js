c4ad88516d5b56675e9b345ed09e280ae71d6c22
Giordon Stark <kratsg@gmail.com>
2016-03-16 20:53:31 -0500

    update auth example

--- config/auth.example.js
+++ config/auth.example.js
@@ -1,17 +1,17 @@
 module.exports = {
-    'facebook' : {
-        'clientID'      : 'your-secret-clientID-here',
-        'clientSecret'  : 'your-client-secret-here',
-        'callbackURL'   : '/auth/facebook/callback'
+    facebook : {
+        clientID      : 'your-secret-clientID-here',
+        clientSecret  : 'your-client-secret-here',
+        callbackURL   : '/auth/facebook/callback'
     },
-    'twitter' : {
-        'consumerKey'       : 'your-consumer-key-here',
-        'consumerSecret'    : 'your-client-secret-here',
-        'callbackURL'       : '/auth/twitter/callback'
+    twitter : {
+        consumerKey       : 'your-consumer-key-here',
+        consumerSecret    : 'your-client-secret-here',
+        callbackURL       : '/auth/twitter/callback'
     },
-    'google' : {
-        'clientID'      : 'your-secret-clientID-here',
-        'clientSecret'  : 'your-client-secret-here',
-        'callbackURL'   : '/auth/google/callback'
+    google : {
+        clientID      : 'your-secret-clientID-here',
+        clientSecret  : 'your-client-secret-here',
+        callbackURL   : '/auth/google/callback'
     }
 };
