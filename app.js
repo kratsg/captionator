545d28b3191e8996294b81f6cc7b47684c8b703e
Giordon Stark <kratsg@gmail.com>
2016-03-16 21:50:24 -0500

    add global user to all views

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next){
     req.yaml = yaml;
     req.fs = fs;
+    res.locals = {user: req.user};
     next();
 });
 
