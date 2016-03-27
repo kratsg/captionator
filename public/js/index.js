fa232e4d23c3b47d680ffa86ed9955c3194a3854
Giordon Stark <kratsg@gmail.com>
2016-03-26 22:22:49 -0500

    no need for these...

--- public/js/index.js
+++
@@ -1,8 +0,0 @@
-var validator = require('jsonschema').validate;
-var yaml = require('js-yaml');
-var schema = require('./schema/play.json');
-
-module.exports = function(blob){
-    var parsedYaml = yaml.safeLoad(blob);
-    return validate(yaml.safeLoad(blob), schema);
-};
