6c371338dc367b320b2c9b9fb6ed2e0b9df44c90
Giordon Stark <kratsg@gmail.com>
2016-05-04 00:45:36 -0500

    this temporarily closes #16

--- services/io.js
+++ services/io.js
@@ -13,28 +13,34 @@ io.on('connection', function (socket) {
     socket.on('subscribe', function(play){
         socket.play = play;
         socket.join(play);
+        console.log('['+new Date()+']  ', ((socket.decoded_token.user !== undefined)?"A":"Una")+"uthorized user joining "+play);
     });
 
     if(socket.decoded_token.user !== undefined){
         socket.on('move to', function(data){
             socket.broadcast.to(socket.play).emit('move to', data);
+            console.log('['+new Date()+']  ', socket.play, "move to", data);
         });
 
         socket.on('toggle blank screen', function(){
             socket.broadcast.to(socket.play).emit('toggle blank screen', {});
+            console.log('['+new Date()+']  ', socket.play, "toggle blank screen");
         });
 
         socket.on('toggle grid', function(){
             socket.broadcast.to(socket.play).emit('toggle grid', {});
+            console.log('['+new Date()+']  ', socket.play, "toggle grid");
         });
 
         socket.on('toggle source code', function(){
             socket.broadcast.to(socket.play).emit('toggle source code', {});
+            console.log('['+new Date()+']  ', socket.play, "toggle source code");
         });
     }
 
     socket.on('connect viewer', function(data){
         socket.broadcast.to(socket.play).emit('connect viewer', {});
+        console.log('['+new Date()+']  ', socket.play, "connect viewer");
     });
 });
 
