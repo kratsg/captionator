fe6d2165226dcb1556ab493107ea1309f7655dc5
Giordon Stark <kratsg@gmail.com>
2017-02-17 01:05:35 -0600

    minor typo, should work now

--- routes/index.js
+++ routes/index.js
@@ -145,7 +145,7 @@ router.get('/download/:playName', isLoggedIn, isExistingPlay, function(req, res,
         s.push(text);    // the string you want
         s.push(null);      // indicates end-of-file basically - the end of the stream
 
-        res.attachment(req.playName+'.yml');
+        res.attachment(req.params.playName+'.yml');
         s.pipe(res);
     });
 });
